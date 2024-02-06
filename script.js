de6b21916fbe5f280b69226458ae58d96f34c150
elisa carboni <elisacarboni@live.com>
2024-02-06 19:49:36 +0100

    fix btn search

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 //? 6a. fetch (GET)
 
-let eminem;
-let metallica;
-let queen;
 
 function loadData(search) {
     fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q="+search)
@@ -12,43 +9,18 @@ function loadData(search) {
 }
 
 
-
-// fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica")
-// .then((response) => response.json()) // Abbiamo una promise restituita qui
-// .then((json) => metallica = json) // Qui abbiamo un oggetto JS (convertito dal JSON ricevuto dall'endpoint)
-// .catch((err) => console.log("Error detected: ", err) );
-
-// fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=queen")
-// .then((response) => response.json()) // Abbiamo una promise restituita qui
-// .then((json) => queen = json) // Qui abbiamo un oggetto JS (convertito dal JSON ricevuto dall'endpoint)
-// .catch((err) => console.log("Error detected: ", err) );
-
-// function cardMusic (json){
-//     let containerCard = document.getElementById("container_card");
-//     let cardImage = document.createElement("div");
-//     cardImage.innerText = json.data[0].title;
-//     // console.log(cardImage);
-//     containerCard.appendChild(cardImage);
-// }
-
 function cardMusic (json){
+
     let artista = document.getElementById("artista");
-    
+    artista.innerHTML= "";
 
     let nameSinger = document.createElement("H2");
     nameSinger.innerText = json.data[0].artist.name;
     artista.appendChild(nameSinger);
 
     let containerCard = document.getElementById("container_card");
+    containerCard.innerHTML = "";
 
-    // for (let i = 0; i < json.data[6]; i++) {
-    //     let cardImage = document.createElement("img");
-    //     cardImage.src = element.album.cover;
-        
-        
-    //     containerCard.appendChild(cardImage);
-    //     console.log(json.data[6]);
-    // }
 
     json.data.forEach(element => {
 
@@ -70,3 +42,11 @@ function cardMusic (json){
 
 
 loadData('eminem');
+
+let btnSearch = document.getElementById("button-search");
+
+btnSearch.addEventListener("click", function(){
+    let imput = document.getElementById("searchField");
+    loadData(imput.value);
+});
+
